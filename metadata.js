d39f4021823bb99131ca1f8ed4993f6589a354a2
DigiDrift <github@digidrift.com>
2019-02-19 06:27:07 +1100

    Added GPL licence

--- metadata.js
+++ metadata.js
@@ -1,5 +1,23 @@
 #!/usr/bin/gjs
 
+/*
+* Nautilus-Metadata a script to show metadata in a GUI on the lLinux desktop
+* Copyright (C) 2019  Jason Webb
+
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 // import standard libraries
 const { Gio, GLib, Gtk, Gdk, Pango, WebKit2 }  = imports.gi;
 const ByteArray = imports.byteArray;
